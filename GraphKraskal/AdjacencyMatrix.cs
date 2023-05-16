50e1af415cc72385264e7ff1d6f1155cacd4f667
Лера Голикова <funtik-98@mail.ru>
2023-05-16 11:24:44 +0700

    Добавьте файлы проекта.

---
+++ GraphKraskal/AdjacencyMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphKraskal
+{
+    internal class AdjacencyMatrix
+    {
+        private int[,] matrix;
+
+        public AdjacencyMatrix(int vertices)
+        {
+            matrix = new int[vertices, vertices];
+        }
+
+        public void AddEdge(int source, int destination, int weight)
+        {
+            matrix[source, destination] = weight;
+            matrix[destination, source] = weight;
+        }
+
+        public void PrintAdjacencyMatrix()
+        {
+            int vertices = matrix.GetLength(0);
+
+            Console.WriteLine("Таблица смежности:");
+
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ GraphKraskal/Edge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphKraskal
+{
+    
+    internal class Edge
+    {
+        public int Source { get; }
+        public int Destination { get; }
+        public int Weight { get; }
+
+        public Edge(int source, int destination, int weight)
+        {
+            Source = source;
+            Destination = destination;
+            Weight = weight;
+        }
+    }
+}
